0b45e42aba2d4c5c6f3aaf22c2c0f3295d060bd8
Jhony Alpala <jhoniher95@gmail.com>
2023-08-30 17:01:51 -0500

    Operaciones finalizadas

--- EjercicioPropuesto12.java
+++ EjercicioPropuesto12.java
@@ -8,12 +8,19 @@ public class EjercicioPropuesto12 {
         
         Scanner entrada=new Scanner(System.in);
         double horas_trabajadas, precio_hora, porcentaje_retencion;
+        double salario_bruto, retencion, salario_neto;
         System.out.println("Ingrese cantidad de horas trabajadas por el empleado: ");
         horas_trabajadas=entrada.nextDouble();
         System.out.println("Ingrese el precio a pagar por hora de trabajo: ");
         precio_hora=entrada.nextDouble();
-        System.out.println("Ingrese el porcentaje de retencion en la fuente: ");
+        System.out.println("Ingrese el porcentaje de retencion en la fuente (0-100): ");
         porcentaje_retencion=entrada.nextDouble();
+        salario_bruto=horas_trabajadas*precio_hora;
+        retencion=salario_bruto*(porcentaje_retencion/100);
+        salario_neto=salario_bruto-retencion;
+        System.out.println("El salario bruto es: "+salario_bruto);
+        System.out.println("La retencion calcuculada sobre el salario es: "+retencion);
+        System.out.println("El salario neto es: "+salario_neto);
         
     }
     
